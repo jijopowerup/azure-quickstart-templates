f2f2d2a4d5e119f98f113218ebbc97b41eec67ea
jijopowerup <joji123abc@gmail.com>
2017-04-21 15:44:41 +0530

    af

--- mysql-replication/azuremysql.sh
+++ mysql-replication/azuremysql.sh
@@ -312,7 +312,7 @@ FLUSH PRIVILEGES;
 EOF
 else
     mysql -u root -p"${ROOTPWD}" <<EOF
-change master to master_host='${MASTERIP}', master_port=3306, master_user='rpluser', master_password='${RPLPWD}', master_auto_position=1;
+change master to master_host='${MASTERIP}', master_port=3306, master_user='rpluser', master_password='${RPLPWD}', master_connect_retry=10;
 START slave;
 EOF
 fi
